919d93dce6adf125ae794f61ef3bc26d33417427
Fabian <donskelle@googlemail.com>
2019-04-28 17:16:08 +0200

    Add styled comps plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   pathPrefix: "/me",
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
