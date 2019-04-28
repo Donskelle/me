7a8db674d53fac1733c671d02b0cefb027fba714
Fabian <donskelle@googlemail.com>
2019-04-28 23:33:23 +0200

    Add Lint

--- src/components/elements.js
+++ src/components/elements.js
@@ -3,7 +3,7 @@ import tw from 'tailwind.macro'
 import { ParallaxLayer } from 'react-spring/renderprops-addons'
 
 export const Content = styled(ParallaxLayer)`
-  ${tw`p-6 md:p-12 lg:p-24 justify-center items-center flex z-50`};
+  ${tw`p-6 md:p-12 lg:p-24 justify-center items-center flex z-50 overflow-y-hidden`};
 `
 export const ContentBG = styled(ParallaxLayer)`
   ${tw`absolute w-full h-full`};
