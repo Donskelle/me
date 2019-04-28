c206bd619be7b0b6f9afe803cb24f49da0e0edef
Fabian <donskelle@googlemail.com>
2019-04-28 16:05:42 +0200

    Edit Prettier Format, Add GH-Pages Deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Donskelle - Frontend Developer Hamburg`,
+    description: `A frontend developer based in Hamburg`,
+    author: `@donskelle`,
   },
+  pathPrefix: "/me",
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
