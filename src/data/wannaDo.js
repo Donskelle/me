d6230ac83a9bfdc349b191cdee7a36d306470119
Fabian Puszies <donskelle@googlemail.com>
2019-08-06 17:53:43 +0200

    Add Trail Animation, Data, Add Intro GitImg hover

---
+++ src/data/wannaDo.js
@@ -0,0 +1,24 @@
+const wannaDoList = [
+  {
+    name: 'Frontend',
+    list: ['React', 'Relay', 'Svelte 3'],
+  },
+  {
+    name: 'State-Managment',
+    list: ['Relay', 'Apollo', 'MobX', 'Redux'],
+  },
+  {
+    name: 'Backend',
+    list: ['Serverless NodeJS', 'Prisma', 'Firebase'],
+  },
+  {
+    name: 'Apps',
+    list: ['PWA', 'React Native', 'Flutter'],
+  },
+  {
+    name: 'CMS-Shop',
+    list: ['JAMstack based with GraphQL'],
+  },
+]
+
+export default wannaDoList
