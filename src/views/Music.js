84721a691cd4b749b490ad40cc6e911644ba2e80
Fabian Puszies <donskelle@googlemail.com>
2019-09-03 22:18:49 +0200

    Add react player

--- src/views/Music.js
+++ src/views/Music.js
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ReactPlayer from 'react-player'
 
 import H2 from '../typo/h2'
 import { Content } from '../components/elements'
-import { useTracks } from '../hooks/runtime/tracks';
+import { useTracks } from '../hooks/runtime/tracks'
 
 const Muzzak = ({ offset }) => {
+  const [url, setUrl] = useState(null)
+  const [playing, setPlaying] = useState(false)
   const tracks = useTracks()
+  // const player = useRef(null)
+
+  useEffect(() => {
+    if (tracks && tracks.length) {
+      const index = tracks.length - 1
+      setUrl(`https://www.youtube.com/watch?v=${tracks[index].youtubeId}`)
+      setPlaying(true)
+    }
+  }, [tracks])
+
   return (
     <Content speed={1} offset={offset}>
       <H2>Add some music here</H2>
       {tracks.map(track => (
-        <div key={track.id}>
-          {track.youtubeId}
-        </div>
+        <div key={track.id}>{track.youtubeId}</div>
       ))}
+      <ReactPlayer playing={playing} url={url} />
     </Content>
   )
 }
