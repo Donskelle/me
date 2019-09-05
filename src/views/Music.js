eeb522ac1d8138aa0abb9c5339b63676a2bb7308
Fabian Puszies <donskelle@googlemail.com>
2019-09-05 10:31:35 +0200

    Add Simple add Youtube ID

--- src/views/Music.js
+++ src/views/Music.js
@@ -1,19 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import ReactPlayer from 'react-player'
+import { API, graphqlOperation } from 'aws-amplify'
 
 import H2 from '../typo/h2'
 import { Content } from '../components/elements'
 import { useTracks } from '../hooks/runtime/tracks'
+import { createTrack } from '../graphql/mutations'
 
 const extendYoutubeUrl = id => `https://www.youtube.com/watch?v=${id}`
 
 const Muzzak = ({ offset }) => {
   const [url, setUrl] = useState(null)
+  const [addYoutubeId, setAddYoutubeId] = useState('')
   const [playing, setPlaying] = useState(false)
   const tracks = useTracks()
 
   const switchTrack = id => {
     setUrl(extendYoutubeUrl(id))
+  }
+
+  const addTrack = () => {
+    if (addYoutubeId.length < 5) return alert('Retard')
+
+    const trackDetail = {
+      youtubeId: addYoutubeId,
+      addedBy: 'Fabian',
+    }
+    API.graphql(graphqlOperation(createTrack, { input: trackDetail }))
+    setAddYoutubeId('')
   }
 
   useEffect(() => {
@@ -28,10 +42,16 @@ const Muzzak = ({ offset }) => {
     <Content speed={1} offset={offset}>
       <H2>Add some music here</H2>
       {tracks.map(track => (
-        <div key={track.id} onClick={() => switchTrack(track.youtubeId)}>
+        <div key={track.id} onClick={() => addTrack(track.youtubeId)}>
           {track.youtubeId}
         </div>
       ))}
+      <input
+        onChange={e => setAddYoutubeId(e.target.value)}
+        placeholder="YoutubeId"
+        value={addYoutubeId}
+      />
+      <button onClick={addTrack}>Add Track</button>
       <ReactPlayer playing={playing} url={url} />
     </Content>
   )
