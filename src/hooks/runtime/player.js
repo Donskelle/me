bf9a5b4ea644357cf96923e257ad29e67585aefb
Fabian Puszies <donskelle@googlemail.com>
2019-09-06 15:30:06 +0200

    Use Remote Player Status

--- src/hooks/runtime/player.js
+++ src/hooks/runtime/player.js
@@ -1,7 +1,43 @@
-// import { useReducer, useEffect } from 'react'
-// import { API, graphqlOperation } from 'aws-amplify'
+import { useEffect, useState } from 'react'
+import { API, graphqlOperation } from 'aws-amplify'
 
+import { onUpdatePlayer as onUpdatePlayerSubsription } from '../../graphql/subscriptions'
+import { getPlayer } from '../../graphql/queries'
 
 export function usePlayer() {
-  
+  const [playerStatus, setPlayerStatus] = useState({
+    playing: false,
+    startTime: null,
+    currentTrack: null,
+  })
+
+  useEffect(() => {
+    API.graphql(
+      graphqlOperation(getPlayer, {
+        id: 'dc3c047f-f0b0-4108-9632-f029440b14b6',
+      }),
+    ).then(playerData => {
+      setPlayerStatus({ ...playerData.data.getPlayer, playing: true })
+      //   dispatch({ type: 'set', payload: tracksData.data.listTracks.items })
+      console.log(playerData)
+    })
+  }, [])
+
+  useEffect(() => {
+    const subscriber = API.graphql(
+      graphqlOperation(onUpdatePlayerSubsription),
+    ).subscribe({
+      next: data => {
+        const {
+          value: {
+            data: { onUpdatePlayer },
+          },
+        } = data
+        setPlayerStatus({ ...onUpdatePlayer, playing: true })
+      },
+    })
+    return () => subscriber.unsubscribe()
+  }, [])
+
+  return playerStatus
 }
--- src/views/Music.js
+++ src/views/Music.js
@@ -5,18 +5,27 @@ import { API, graphqlOperation } from 'aws-amplify'
 import H2 from '../typo/h2'
 import { Content } from '../components/elements'
 import { useTracks } from '../hooks/runtime/tracks'
-import { createTrack } from '../graphql/mutations'
+import { createTrack, updatePlayer } from '../graphql/mutations'
+import { usePlayer } from '../hooks/runtime/player'
 
 const extendYoutubeUrl = id => `https://www.youtube.com/watch?v=${id}`
 
 const Muzzak = ({ offset }) => {
   const [url, setUrl] = useState(null)
   const [addYoutubeId, setAddYoutubeId] = useState('')
-  const [playing, setPlaying] = useState(false)
+  // const [playing, setPlaying] = useState(false)
   const tracks = useTracks()
+  const { status, currentTrack, startTime, playing } = usePlayer()
 
   const switchTrack = id => {
-    setUrl(extendYoutubeUrl(id))
+    API.graphql(
+      graphqlOperation(updatePlayer, {
+        input: {
+          playerCurrentTrackId: id,
+          id: 'dc3c047f-f0b0-4108-9632-f029440b14b6',
+        },
+      }),
+    )
   }
 
   const addTrack = () => {
@@ -31,18 +40,25 @@ const Muzzak = ({ offset }) => {
   }
 
   useEffect(() => {
-    if (tracks && tracks.length) {
-      const index = tracks.length - 1
-      switchTrack(tracks[index].youtubeId)
-      setPlaying(true)
+    if (currentTrack) {
+      setUrl(extendYoutubeUrl(currentTrack.youtubeId))
     }
-  }, [tracks])
+    console.log(playing, currentTrack, startTime)
+  }, [playing, currentTrack, startTime])
+
+  // useEffect(() => {
+  //   if (tracks && tracks.length) {
+  //     const index = tracks.length - 1
+  //     switchTrack(tracks[index].youtubeId)
+  //     setPlaying(true)
+  //   }
+  // }, [tracks])
 
   return (
     <Content speed={1} offset={offset}>
       <H2>Add some music here</H2>
       {tracks.map(track => (
-        <div key={track.id} onClick={() => switchTrack(track.youtubeId)}>
+        <div key={track.id} onClick={() => switchTrack(track.id)}>
           {track.youtubeId}
         </div>
       ))}
