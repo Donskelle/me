cd4a344b703d2f750762a6c67f5550ebb902a671
Fabian Puszies <donskelle@googlemail.com>
2019-09-27 23:17:38 +0530

    update amp

--- amplify/backend/function/youtubesearch/src/index.js
+++ amplify/backend/function/youtubesearch/src/index.js
@@ -1,29 +1,29 @@
 const axios = require('axios')
 
-exports.handler = (event, context) => {
-  
+exports.handler = (event, context, callback) => {
   const searchString = event.arguments ? event.arguments.search : 'Captial Bra'
   axios
     .get(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(searchString)}&key=${
-        process.env.YOUTUBE_APIKEY
-      }`,
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(
+        searchString,
+      )}&key=${process.env.YOUTUBE_APIKEY}`,
     )
     .then(response => {
-      const videos = response.data.items.map(({snippet, id}) => ({
-        youtubeId: id.videoId,
-        channelTitle: snippet.channelTitle,
-        title: snippet.title,
-        publishedAt: snippet.publishedAt,
-        description: snippet.description,
-        thumbnails: snippet.thumbnails
-      }))
+      const videos = response.data.items
+        .filter(({ snippet, id }) => snippet && id)
+        .map(({ snippet, id }) => ({
+          youtubeId: id.videoId,
+          channelTitle: snippet.channelTitle,
+          title: snippet.title,
+          publishedAt: snippet.publishedAt,
+          description: snippet.description,
+          thumbnails: snippet.thumbnails,
+        }))
 
       context.done(null, videos)
     })
     .catch(e => {
-      console.log(e)
       // handle error
-      context.done(null, [])
+      callback(e)
     })
 }
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -5,7 +5,7 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://q7cjpf7cz5eihh7opc5yqz5n3a.appsync-api.eu-west-2.amazonaws.com/graphql",
     "aws_appsync_region": "eu-west-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-vmub5aaqyvdovcyyydpkluygwe"
+    "aws_appsync_apiKey": "da2-x7gceeeso5bmfcqggcf2m5gq7u"
 };
 
 
