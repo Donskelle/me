2f1f1b1df989284a36aacedf84c326a433fb2191
Fabian Puszies <donskelle@googlemail.com>
2020-09-01 23:51:04 +0200

    refactor: remove comment

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
 const { createRemoteFileNode } = require(`gatsby-source-filesystem`);
 
 // Extend Github Repository Resolver.
-// Creates Local File of Profile Pics to optimze via gatsby image
+// Creates Local of Profile Pics to optimze via gatsby image
 exports.createResolvers = async ({
   actions,
   cache,
@@ -19,7 +19,6 @@ exports.createResolvers = async ({
         async resolve(source) {
           const { avatarUrl } = source.owner;
 
-          // eslint-disable-next-line no-return-await
           return createRemoteFileNode({
             url: encodeURI(avatarUrl),
             store,
